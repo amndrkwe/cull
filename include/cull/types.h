961d505c3a881eb218ec9f2cd9a2ef14cb89b88c
amndrkwe <amndrkwe@gmail.com>
2021-08-30 12:08:10 -0300

    renamed int.h to types.h

--- include/cull/types.h
+++ include/cull/types.h
@@ -15,16 +15,17 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
+// check is float type follows the ieee standard for 32 bit floats
 #if FLT_MANT_DIG == 24 && FLT_MAX_EXP == 128
     typedef float f32;
-#elif FLT_MANT_DIG == 53 && FLT_MAX_EXP == 1024
-    typedef float f64;
 #endif
 
+// same thing but for doubles
 #if DBL_MANT_DIG == 53 && DBL_MAX_EXP == 1024
     typedef double f64;
 #endif
 
+// same thing but for long doubles
 #if LDBL_MANT_DIG == 64 && LDBL_MAX_EXP == 16384
     typedef long double f80;
 #endif

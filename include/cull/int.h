4df7381ab70b54cc99cbfaf94cf767e0f03d2574
amndrkwe <amndrkwe@gmail.com>
2021-08-30 11:44:08 -0300

    added float types to int.h

--- include/cull/int.h
+++ include/cull/int.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
-
-#ifndef CULL_IGNORE_INT
+#include <float.h>
+#include <limits.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -15,4 +15,16 @@ typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
 
+#if FLT_MANT_DIG == 24 && FLT_MAX_EXP == 128
+    typedef float f32;
+#elif FLT_MANT_DIG == 53 && FLT_MAX_EXP == 1024
+    typedef float f64;
+#endif
+
+#if DBL_MANT_DIG == 53 && DBL_MAX_EXP == 1024
+    typedef double f64;
+#endif
+
+#if LDBL_MANT_DIG == 64 && LDBL_MAX_EXP == 16384
+    typedef long double f80;
 #endif
